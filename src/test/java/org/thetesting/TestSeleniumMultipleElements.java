e17e229624864d193df8d3bc2446315c55583db2
Rachna Kumari <rachnakumari0320@gmail.com>
2026-06-04 22:10:08 +0530

    Automate multiple element handling using findElements

---
+++ src/test/java/org/thetesting/TestSeleniumMultipleElements.java
@@ -0,0 +1,28 @@
+package org.thetesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+    public class TestSeleniumMultipleElements {
+        @Test
+        public void verifyNumberOfLinks() {
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://the-internet.herokuapp.com/");
+
+            List<WebElement> links = driver.findElements(By.tagName("a"));
+            System.out.println("Total Links: " + links.size());
+
+            Assert.assertTrue(links.size() > 0);
+            driver.quit();
+        }
+    }
+
+
+
