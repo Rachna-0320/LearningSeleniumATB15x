2dcca5bda7e7a6fada79bf08d2ce1df86926e7c4
Rachna Kumari <rachnakumari0320@gmail.com>
2026-06-20 22:41:30 +0530

    Automate logout functionality and URL validation

--- src/test/java/org/thetesting/TestLogoutFunctionality.java
+++ src/test/java/org/thetesting/TestLogoutFunctionality.java
@@ -6,30 +6,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestSuccessfulLogin {
+public class TestLogoutFunctionality {
     @Test
-    public void verifySuccessfulLogin() {
+    public void verifyLogout() {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/login");
 
-        // Enter valid credentials
+        // Login
         driver.findElement(By.id("username"))
                 .sendKeys("tomsmith");
 
         driver.findElement(By.id("password"))
                 .sendKeys("SuperSecretPassword!");
 
-        // Click Login
         driver.findElement(By.cssSelector("button[type='submit']"))
                 .click();
 
-        // Verify success message
-        String message = driver.findElement(By.id("flash"))
-                .getText();
+        // Click Logout
+        driver.findElement(By.cssSelector("a.button.secondary.radius"))
+                .click();
 
-        System.out.println(message);
-        Assert.assertTrue(message.contains("You logged into a secure area!"));
+        // Verify user is back on login page
+        Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
+        System.out.println("Logout Successful!");
         driver.quit();
     }
 }
+
